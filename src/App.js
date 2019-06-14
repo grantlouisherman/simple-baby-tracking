289171c72fc8fd26fc51cad5e732714bd286f959
Grant Herman <grantlouisherman041@gmail.com>
2019-06-14 10:32:59 -0700

    build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Button from './Components/Button'
 import TimeSeries  from './Components/TimeSeries'
