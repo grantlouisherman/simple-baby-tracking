f49058d1cccd6107d1f7696cb78c8700349278dd
Grant Herman <grantlouisherman041@gmail.com>
2019-06-14 18:06:56 -0700

    Updates

---
+++ precache-manifest.46f3464d38b8be3a4153681e03f7e667.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97273ce45fba8c7991658288f7284c6",
+    "url": "/simple-baby-tracking/index.html"
+  },
+  {
+    "revision": "dfe03a14b03ae134f40b",
+    "url": "/simple-baby-tracking/static/css/main.acbdc72b.chunk.css"
+  },
+  {
+    "revision": "217b4f6c539e1dbe164b",
+    "url": "/simple-baby-tracking/static/js/2.0603af4e.chunk.js"
+  },
+  {
+    "revision": "dfe03a14b03ae134f40b",
+    "url": "/simple-baby-tracking/static/js/main.7c012a2d.chunk.js"
+  },
+  {
+    "revision": "b7fa6c36d15c92f41682",
+    "url": "/simple-baby-tracking/static/js/runtime~main.000e4589.js"
+  }
+]);
